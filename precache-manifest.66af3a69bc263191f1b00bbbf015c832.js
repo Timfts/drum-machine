bd3f71dae051add48ff817a53c2f5317ab817760
italo fontes <italo5123@hotmail.com>
2019-02-16 11:21:12 -0200

    Updates

---
+++ precache-manifest.66af3a69bc263191f1b00bbbf015c832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e17d149b71a3851c5955",
+    "url": "/drum-machine/static/css/main.e8c0dd39.chunk.css"
+  },
+  {
+    "revision": "e17d149b71a3851c5955",
+    "url": "/drum-machine/static/js/main.e17d149b.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "/drum-machine/static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-machine/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3319765e8996b7ccb737533415b4f16",
+    "url": "/drum-machine/index.html"
+  }
+];
